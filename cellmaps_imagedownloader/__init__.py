0187bae55571c99a8f35c37df7843c518ec898f8
Chris Churas <churas.camera@gmail.com>
2023-08-14 11:07:07 -0700

    bumped version adjusted author

--- cellmaps_imagedownloader/__init__.py
+++ cellmaps_imagedownloader/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for cellmaps_imagedownloader."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a7'
+__version__ = '0.1.0a8'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_imagedownloader'
 __description__ = 'A tool to download specified image data from the Human Protein Atlas website using CSV file'
